e13a10c2a25cbfeac4686136fc46d58728bd9dda
JoinHearthstone <joinhearthstone@outlook.es>
2017-02-23 11:13:34 +0100

    GildedRoseInn_Empty ready for defining and implementing features

---
+++ GildedRoseInn/target/test-classes/features/hello.feature
@@ -0,0 +1,9 @@
+Feature: Hello Cucumber
+	As a cucumber student
+	I want a hello world scenario 
+	In order to learn the basics of cucumber
+	
+Scenario:
+	Given a student called "Bob"
+	When Bob says hello to Cucumber
+	Then Cucumber answer "Hello Bob" 
---
+++ GildedRoseInn_Empty/src/test/java/stepsimpl/HelloCucumberSteps.java
@@ -0,0 +1,32 @@
+package stepsimpl;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+import hello.MyCucumber;
+
+import static org.junit.Assert.*;
+
+public class HelloCucumberSteps {
+
+	String name = "Unset";
+	String result = "Unset";
+	
+	@Given("^a student called \"([^\"]*)\"$")
+	public void a_student_called(String name) throws Throwable {
+	   this.name = name;
+	}
+
+	@When("^Bob says hello to Cucumber$")
+	public void bob_says_hello_to_Cucumber() throws Throwable {
+		MyCucumber cucumber = new MyCucumber();
+	    this.result = cucumber.hello(this.name);
+	}
+
+	@Then("^Cucumber answer \"([^\"]*)\"$")
+	public void cucumber_answer(String msg) throws Throwable {
+	   assertEquals(this.result, msg);
+	}
+
+
+	
+}
