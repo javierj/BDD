899a87989cae213a1c225b679f54e1e8852abd38
JoinHearthstone <joinhearthstone@outlook.es>
2017-03-05 16:07:04 +0100

    Solved a problem with a folder linked into two projects

--- Hello_Cucumber/src/test/java/RunCukesTest.java
+++ Hello_Cucumber/src/test/java/RunCukesTest.java
@@ -1,11 +1,7 @@
-//package acceptance;
-
 
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions()
 public class RunCukesTest {
 }
